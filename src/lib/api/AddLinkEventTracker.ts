b3e2b970e6ac0d542ce7bd6947fe8c9deaa70501
himalayo <himalayo@protonmail.com>
2026-01-18 08:44:19 +0000

    feat: add link event trackers

---
+++ src/lib/api/AddLinkEventTracker.ts
@@ -0,0 +1,6 @@
+import { type ILinkEventTracker } from '@nitrots/nitro-renderer';
+import { GetNitroInstance } from './GetNitroInstance';
+
+export function AddEventLinkTracker(tracker: ILinkEventTracker): void {
+	GetNitroInstance().addLinkEventTracker(tracker);
+}
