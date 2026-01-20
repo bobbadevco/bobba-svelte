c68757a51c2031071e1131d24e79a1e630aa32b1
himalayo <himalayo@protonmail.com>
2026-01-20 18:08:04 +0000

    style: remove unnecessary effect

--- src/lib/themes/default/views/navigator/NavigatorView.svelte
+++ src/lib/themes/default/views/navigator/NavigatorView.svelte
@@ -6,51 +6,47 @@
 
 	let isVisible = $state(false);
 
-	$effect(() => {
-		const linkTracker: ILinkEventTracker = {
-			linkReceived: (url: string) => {
-				const parts = url.split('/');
+	const linkTracker: ILinkEventTracker = {
+		linkReceived: (url: string) => {
+			const parts = url.split('/');
 
-				let method = parts[0];
-				let value = parts.length > 1 ? parts[1] : undefined;
+			let method = parts[0];
+			let value = parts.length > 1 ? parts[1] : undefined;
 
-				if (method === 'navigator' && parts.length > 1) {
-					method = parts[1];
-					value = parts.length > 2 ? parts[2] : undefined;
-				}
+			if (method === 'navigator' && parts.length > 1) {
+				method = parts[1];
+				value = parts.length > 2 ? parts[2] : undefined;
+			}
 
-				switch (method) {
-					case 'show':
-						isVisible = true;
-						return;
-					case 'hide':
-						isVisible = false;
-						return;
-					case 'toggle':
-						isVisible = !isVisible;
-						return;
-					case 'goto':
-						if (!value) return;
+			switch (method) {
+				case 'show':
+					isVisible = true;
+					return;
+				case 'hide':
+					isVisible = false;
+					return;
+				case 'toggle':
+					isVisible = !isVisible;
+					return;
+				case 'goto':
+					if (!value) return;
 
-						if (value === 'home') {
-							if (getHomeRoomId() > 0) TryVisitRoom(getHomeRoomId());
-						} else {
-							const roomId = parseInt(value);
-							if (!isNaN(roomId)) TryVisitRoom(roomId);
-						}
-						return;
-				}
-			},
-			eventUrlPrefix: 'navigator/',
-		};
-
-		AddEventLinkTracker(linkTracker);
-
-		return () => RemoveLinkEventTracker(linkTracker);
-	});
+					if (value === 'home') {
+						if (getHomeRoomId() > 0) TryVisitRoom(getHomeRoomId());
+					} else {
+						const roomId = parseInt(value);
+						if (!isNaN(roomId)) TryVisitRoom(roomId);
+					}
+					return;
+			}
+		},
+		eventUrlPrefix: 'navigator/',
+	};
 
 	onMount(() => {
 		registerNavigatorEvents();
+		AddEventLinkTracker(linkTracker);
+		return () => RemoveLinkEventTracker(linkTracker);
 	});
 </script>
 <div class="absolute bottom-0 left-0">
