8076d4df0ecf7910e623fa05034b963bc70a92da
himalayo <himalayo@protonmail.com>
2026-01-20 22:33:29 +0000

    feat: draggable window improvements + remove user-select for all components

--- src/lib/themes/default/generic/DraggableWindow.svelte
+++ src/lib/themes/default/generic/DraggableWindow.svelte
@@ -3,46 +3,22 @@
 	import type { ClassValue } from 'svelte/elements';
 	import type { Snippet } from 'svelte';
 
-	let {visible = $bindable(false), class: classes = '', children = undefined}: { visible?: boolean, class?: ClassValue, children?: Snippet } = $props();
+	let {visible = $bindable(false), title='', class: classes = '', children = undefined}: { visible?: boolean, class?: ClassValue, children?: Snippet, title: string } = $props();
 
 	let x = $state(0);
 	let y = $state(0);
 	let width = $state(0);
 	let height = $state(0);
-	let resizing = $state(false);
-
-	function onmousedown() {
-		resizing = true;
-	}
-
-	function onmouseup() {
-		resizing = false;
-	}
-
-	function onmousemove(e: MouseEvent) {
-		if (resizing) {
-			width += e.movementX;
-			height += e.movementY;
-		}
-	}
-
-	function onmouseout() {
-		resizing = false;
-	}
-	function onblur() {}
 </script>
 
 {#if visible}
 	<div style:width="{width}px" style:height="{height}px" style:left="{x}px" style:top="{y}px" class={["absolute flex flex-col min-w-fit min-h-fit border border-red-500", classes]} bind:clientWidth={null, (w) => w && w >= width ? width = w : null} bind:clientHeight={null, (h) =>h && h >= height ? height = h : null}>
 		<Draggable bind:x={x} bind:y={y} class="h-20 w-full flex bg-black text-white flex-row justify-between items-center px-4 border border-blue-500">
-			test
+			{title}
 			<button class="rounded-md border bg-rose-800 text-white p-2" type="button" onclick={() => visible = false}>x</button>
 		</Draggable>
 
 		{@render children?.()}
-
-		<div tabindex="-1" role="button" class="absolute -bottom-5 -right-5 size-10 cursor-se-resize" {onblur} {onmousedown} {onmouseup} {onmousemove} {onmouseout}>
-
-		</div>
+		<Draggable bind:x={width} bind:y={height} class="absolute -bottom-5 -right-5 cursor-se-resize size-10" />
 	</div>
 {/if}
