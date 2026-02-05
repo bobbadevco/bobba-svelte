29a658771fc354f945cad69ab4b52387658994be
Key <dkeyalonsor@gmail.com>
2026-02-05 07:23:20 +0100

    feat(chatinput): Moved user's head to left chatbar

--- src/lib/themes/default/views/toolbar/ChatInputComponent.svelte
+++ src/lib/themes/default/views/toolbar/ChatInputComponent.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
-
 import ChatInputWidget from '$lib/components/widgets/ChatInputWidget.svelte';
-import { getRoomSession } from '$lib/events';
+import { getRoomSession, getUserLook } from '$lib/events';
+import Button from '$lib/components/common/Button.svelte';
+import AvatarImage from '$lib/components/common/layout/AvatarImage.svelte';
+import Flex from '$lib/components/common/Flex.svelte';
+
+const userLook = $derived(getUserLook());
 </script>
 
 {#if getRoomSession()}
-	<div class="absolute bottom-2 w-full flex">
-		<ChatInputWidget type="text" class="w-100 p-2 bg-white rounded-md border-black border-2 focus:outline-0 text-black mx-auto" />
-	</div>
+	<Flex class="absolute bottom-2 w-full">
+		<Flex class="ms-3 me-auto gap-2 items-center">
+			<Button
+				id="avatar-image-button"
+				class="hover:-translate-y-0.5 hover:drop-shadow-[0px_2px_#00000080] w-13 border-[3px] outline-black outline-2 rounded-full border-solid overflow-hidden border-bright-primary bg-tertiary h-13 relative" center>
+				<AvatarImage class="absolute -bottom-4" headOnly figure={ userLook }/>
+			</Button>
+			<ChatInputWidget type="text" class="w-100 h-11 p-2 bg-white rounded-md border-black border-2 focus:outline-0 text-black mx-auto" />
+		</Flex>
+	</Flex>
 {/if}
--- src/lib/themes/default/views/toolbar/ToolbarLeftComponent.svelte
+++ src/lib/themes/default/views/toolbar/ToolbarLeftComponent.svelte
@@ -8,14 +8,13 @@
 	import Fa from "svelte-fa";
     
 	let isVisible = $state(false);
-	const userLook = $derived(getUserLook());
 </script>
 
 
 <Flex center class="absolute z-1 transition-all duration-300 top-[30vh] {isVisible ? '-left-18.25' : 'left-0'}">
 	<Flex column class="w-18">
-		<Flex class="w-full bg-tertiary shadow-[1px_0px_black,0px_-1px_black] border-t-2 border-r-2 border-solid border-bright-primary rounded-tr-full h-9" />
-		<Flex center column class="bg-tertiary border-r-2 border-solid border-bright-primary shadow-[1px_0px_black] gap-2">
+		<Flex class="w-full bg-tertiary shadow-[1px_0px_black,0px_-1px_black] border-t-2 border-r-2 border-solid border-bright-primary rounded-tr-full h-7.5" />
+		<Flex center column class="bg-tertiary border-r-2 border-solid border-bright-primary shadow-[1px_0px_black] gap-3 justify-center items-center w-full">
 			<Flex id="hotel-view" center class="w-full h-7.5">
 				{#if !getRoomSession()}
 					<Button 
@@ -42,18 +41,13 @@
 			<Button 
 				id="camera-button" 
 				class="bg-(image:--toolbar-spritesheet) hover:-translate-0.5 w-9.75 h-11.25 bg-position-[-38px_0px]" />
-			<Button 
-				id="avatar-image-button"
-				class="hover:-translate-y-0.5 hover:drop-shadow-[0px_2px_black] w-13 border-[3px] outline-black outline-2 rounded-full border-solid overflow-hidden border-bright-primary bg-tertiary h-13 relative" center>
-				<AvatarImage class="absolute -bottom-4" headOnly figure={ userLook }/>
-			</Button>
 		</Flex>
-		<Flex class="w-full bg-tertiary shadow-[1px_0px_black,0px_1px_black] border-b-2 border-r-2 border-solid border-bright-primary rounded-br-full h-9" />
+		<Flex class="w-full bg-tertiary shadow-[1px_0px_black,0px_1px_black] border-b-2 border-r-2 border-solid border-bright-primary rounded-br-full h-7.5" />
 	</Flex>
 	<Button 
-		class="shadow-[1px_0px_black,0px_-1px_black,0px_1px_black] px-1.5 right-0.5 rounded-r-md border-2 border-l-0 border-solid border-bright-primary relative bg-tertiary h-15" 
+		class="shadow-[1px_0px_black,0px_-1px_black,0px_1px_black] px-1.5 right-0.5 rounded-r-md border-2 border-l-0 border-solid border-bright-primary relative bg-tertiary h-15"
 		onclick={() => isVisible = !isVisible} center>
-		<Fa color="white" class="size-full {isVisible ? 'rotate-180 transition-transform duration-300' : 'rotate-0 transition-transform duration-300'}" icon={ faAngleRight } />
+		<Fa color="white" class="size-full {isVisible ? 'rotate-0 transition-transform duration-300' : 'rotate-180 transition-transform duration-300'}" icon={ faAngleRight } />
 	</Button>
 </Flex>
 
