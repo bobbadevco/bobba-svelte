1512c582c5613d431c2b1c85ff91c15b7d075b6d
Key <dkeyalonsor@gmail.com>
2026-02-05 06:58:17 +0100

    update(chat): Chat toolbar

--- src/lib/themes/default/views/toolbar/ChatInputComponent.svelte
+++ src/lib/themes/default/views/toolbar/ChatInputComponent.svelte
@@ -5,7 +5,7 @@ import { getRoomSession } from '$lib/events';
 </script>
 
 {#if getRoomSession()}
-	<div class="absolute bottom-0 w-full flex">
-		<ChatInputWidget type="text" class="w-100 p-4 bg-white rounded-lg text-black mx-auto" />
+	<div class="absolute bottom-2 w-full flex">
+		<ChatInputWidget type="text" class="w-100 p-2 bg-white rounded-md border-black border-2 focus:outline-0 text-black mx-auto" />
 	</div>
 {/if}
