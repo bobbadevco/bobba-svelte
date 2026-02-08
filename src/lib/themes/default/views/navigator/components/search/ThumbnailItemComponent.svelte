e0ca0e468d83cca2afab4f99da904b3415d16953
Dackar <dkeyalonsor@gmail.com>
2026-02-08 20:00:56 +0100

    fix: (Thumnail View)

--- src/lib/themes/default/views/navigator/components/search/ThumbnailItemComponent.svelte
+++ src/lib/themes/default/views/navigator/components/search/ThumbnailItemComponent.svelte
@@ -13,15 +13,34 @@
 	}
 
 	let {roomData}: ThumbnailItemProps = $props();
+
 	const navigator = getNavigatorListener();
 	const visitRoom = () => navigator.visitRoom(roomData);
+
+	const occupancyColor = () =>
+	{
+		if (roomData.userCount === 0){
+			return '#959595';
+		}
+		const percentage = roomData.userCount / roomData.maxUserCount;
+
+		if (percentage <= 0.5){
+			return '#269900';
+		}
+		if (percentage <= 0.9){
+			return '#cda100';
+		}
+		return '#cd0000';
+	};
 </script>
 
 <Flex class="bg-secondary rounded-md p-2 max-w-31.5" onclick={visitRoom} pointer column >
 	<RoomThumbnail {defaultImage} roomId={roomData.roomId} customUrl={roomData.officialRoomPicRef} class="flex bg-tertiary rounded-sm flex-col items-center justify-end mb-1">
-		<Flex center class="absolute m-1 p-1">
-			<Fa icon={faUser} />
-			<span class="ml-1 text-xs">{roomData.userCount}</span>
+		<Flex class="justify-center items-center rounded-sm p-0 px-2 mb-1 gap-2 font-bold" style="background-color: {occupancyColor()}">
+			<div class="bg-(image:--navigator-spritesheet) bg-position-[-50px_-58px] w-1.75 h-2 bg-no-repeat"></div>
+			<p class="text-[12px]">
+				{roomData.userCount}
+			</p>
 		</Flex>
 	</RoomThumbnail>
 	<Flex fullWidth>
